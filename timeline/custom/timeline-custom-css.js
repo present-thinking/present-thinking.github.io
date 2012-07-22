1887d30862fdf9dc071fcc66e7ccaac2967d3bee
Robbie McCorkell <robbie@presentthinking.net>
2012-07-22 18:57:08 +0100

    Added timeline to the homepage, and added custom styles. Still needs tweaking.

---
+++ timeline/custom/timeline-custom-css.js
@@ -0,0 +1,4 @@
+var fileref=document.createElement("link")
+  fileref.setAttribute("rel", "stylesheet")
+  fileref.setAttribute("type", "text/css")
+  fileref.setAttribute("href", "timeline-edits.css")
---
+++ timeline/custom/timeline-edits.js
@@ -0,0 +1,6 @@
+$(function(){
+
+	var timeline = new VMM.Timeline();
+	timeline.init("https://docs.google.com/spreadsheet/pub?key=0AmStie9KGcuFdGgxR20wTnBfN3Y2UWMxUnlRSV9XWnc&output=html");
+
+});
